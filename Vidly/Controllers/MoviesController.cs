f97a41b9386912dca76ade9444955842696efbe8
Viacheslav <maker1992@mail.ru>
2020-10-13 20:53:54 +0300

    Add functionality to movies menu.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,15 +1,28 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
-        public ActionResult Random()
+        private static List<Movie> sMovies;
+        public MoviesController()
         {
-            var movie = new Movie() { Name = "Star Wars" };
-            return View(movie);
+            sMovies = new List<Movie>();
+            Movie movie = new Movie() { Id = 1, Name = "Star Wars" };
+            sMovies.Add(movie);
+            movie = new Movie() { Id = 1, Name = "James Bond" };
+            sMovies.Add(movie);
+            movie = new Movie() { Id = 1, Name = "Shrek" };
+            sMovies.Add(movie);
         }
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View(sMovies);
+        }
+
     }
 }
