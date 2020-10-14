3b4ccbbb879da761eb824156e2ad3ad3bafa202c
Viacheslav <maker1992@mail.ru>
2020-10-14 11:43:06 +0300

    Add birthday info to customer profile page.

--- Vidly/Migrations/202010140834236_AddBirthdayFieldToCustomers.cs
+++ Vidly/Migrations/202010140834236_AddBirthdayFieldToCustomers.cs
@@ -1,15 +1,14 @@
 namespace Vidly.Migrations
 {
-    using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class AddBirthdayFieldToCustomers : DbMigration
     {
         public override void Up()
         {
             AddColumn("dbo.Customers", "Birthday", c => c.DateTime(nullable: false));
         }
-        
+
         public override void Down()
         {
             DropColumn("dbo.Customers", "Birthday");
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -10,7 +10,8 @@ namespace Vidly.Models
         [Required]
         [StringLength(100)]
         public string Name { get; set; }
-        public DateTime Birthday { get; set; }
+
+        public DateTime? Birthday { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
